a44aa4027bc28d47c83164e250efbafc5ed6ff6e
Arihant Pandey <arihantpandey2002@gmail.com>
2024-01-08 05:11:34 +0000

    add my detection from jetson inference

--- advanced_kb_dist.py
+++ advanced_kb_dist.py
@@ -93,7 +93,6 @@ print("listening keyboard")
 listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 listener.start()
 
-# Read from Arduino in a loop
 readFromArduino()
 
 ser.close
