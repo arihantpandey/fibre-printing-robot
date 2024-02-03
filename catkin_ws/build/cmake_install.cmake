ec5f7ee6053e38b8adef10cfca20ff02047c8f59
Arihant Pandey <arihantpandey2002@gmail.com>
2024-02-03 16:41:47 +0000

    ros additions:

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -132,6 +132,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/fpr/fibre-printing-robot/catkin_ws/build/gtest/cmake_install.cmake")
   include("/home/fpr/fibre-printing-robot/catkin_ws/build/ros_deep_learning/cmake_install.cmake")
+  include("/home/fpr/fibre-printing-robot/catkin_ws/build/learning_tf/cmake_install.cmake")
 
 endif()
 
